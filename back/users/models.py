561ac02deafe229a826f71ff4da3c119ec279ee3
Kdelphinus <kmj951015@gmail.com>
2024-02-14 22:28:04 +0900

    :art: db의 테이블 이름을 Users로 명시

--- back/users/models.py
+++ back/users/models.py
@@ -1,3 +1,4 @@
+import uuid
 from django.db import models
 
 
@@ -7,12 +8,16 @@ class UserStatusEnum(models.TextChoices):
 
 
 class Users(models.Model):
-    user_id = models.UUIDField(primary_key=True)
+    user_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     intra_id = models.CharField(max_length=20)
     nickname = models.CharField(max_length=20)
-    profile_image = models.ImageField()
+    profile_image = models.ImageField(null=True, blank=True) # https://django-orm-cookbook-ko.readthedocs.io/en/latest/null_vs_blank.html
     win_count = models.IntegerField(default=0)
     lose_count = models.IntegerField(default=0)
     created_time = models.DateTimeField(auto_now_add=True)  # 생성 시간 자동 설정
     updated_time = models.DateTimeField(auto_now=True)      # 업데이트 시간 자동 설정
     status = models.CharField(max_length=2, choices=UserStatusEnum.choices)  # max_length 수정
+
+    class Meta:
+        db_table = "Users"
+        ordering = ['created_time']
