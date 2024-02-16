f953a4a0b08883cbb5fa96ee7af6f352e39b5d72
Kdelphinus <kmj951015@gmail.com>
2024-02-16 12:02:16 +0900

    :fire: item 삭제

--- back/item/views.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import viewsets
-from .serializers import ItemSerializer
-from .models import Item
-
-
-class ItemViewSet(viewsets.ModelViewSet):
-    queryset = Item.objects.all()
-    serializer_class = ItemSerializer
