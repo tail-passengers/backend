d5c92372cd38810f52d126dc7a1235be15dfc5c8
Kdelphinus <kmj951015@gmail.com>
2024-02-18 18:01:50 +0900

    :zap: User의 profile_image가 /app/media/profile_image 경로에 저장되도록 설정
    Signed-off-by: Kdelphinus <kmj951015@gmail.com>

--- back/users/models.py
+++ back/users/models.py
@@ -11,7 +11,7 @@ class Users(models.Model):
     user_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     intra_id = models.CharField(max_length=20)
     nickname = models.CharField(max_length=20)
-    profile_image = models.ImageField(null=True, blank=True)
+    profile_image = models.ImageField(null=True, blank=True, upload_to="profile_images")
     win_count = models.IntegerField(default=0)
     lose_count = models.IntegerField(default=0)
     created_time = models.DateTimeField(auto_now_add=True)  # 생성 시간 자동 설정
