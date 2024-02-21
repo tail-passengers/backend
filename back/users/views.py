d8060f620cf47115c62d9d453c8bd81a15028aff
Kdelphinus <kmj951015@gmail.com>
2024-02-21 21:27:11 +0900

    :zap: profile_image 저장 및 db 저장 방식을 모델을 생성하는 방식으로 변경
    Signed-off-by: Kdelphinus <kmj951015@gmail.com>

--- back/users/views.py
+++ back/users/views.py
@@ -4,9 +4,8 @@ import uuid
 from rest_framework import viewsets
 from rest_framework.views import APIView
 from django.shortcuts import redirect
+from django.core.files.base import ContentFile
 from dotenv import load_dotenv
-from .serializers import UsersSerializer
-from .models import Users
 from .serializers import UsersSerializer, FriendRequestSerializer
 from .models import Users, FriendRequests
 
@@ -31,14 +30,15 @@ class Login42APIView(APIView):
 
         oauth_42_api_url = "https://api.intra.42.fr/oauth/authorize"
         return redirect(
-            f'{oauth_42_api_url}?client_id={client_id}&redirect_uri={redirect_uri}&response_type={response_type}&state={state}'
+            f"{oauth_42_api_url}?client_id={client_id}&redirect_uri={redirect_uri}&response_type={response_type}&state={state}"
         )
 
 
+# https://soyoung-new-challenge.tistory.com/92
 class CallbackAPIView(APIView):
     def get(self, request, *args, **kwargs):
         access_token = self._get_access_token(request)
-        #42 api에 정보 요청
+        # 42 api에 정보 요청
         user_info_request = requests.get(
             "https://api.intra.42.fr/v2/me",
             headers={"Authorization": f"Bearer {access_token}"},
@@ -46,25 +46,37 @@ class CallbackAPIView(APIView):
         user_info = user_info_request.json()
         login_id = user_info["login"]
         image_address = user_info["image"]["versions"]["small"]
-        db_request_data = {"intra_id": login_id, "nickname": login_id, "win_count": 0,
-                           "lose_count": 0, "status": 0}  # , "profile_image": image_address}
 
-        # post 요청을 통해 db 업데이트 (프로필 이미지 업로드가 아직 불가)
-        token_request = requests.post("http://127.0.0.1:8000/Users/", db_request_data)
-        return redirect(
-            "http://127.0.0.1:8000/Users/"
+        # image 저장을 위해 post 말고 모델을 생성해서 저장
+        # win, lose는 기본값이 0이므로 따로 저장하지 않음
+        user_instance = Users(
+            intra_id=login_id,
+            nickname=login_id,
+            status=0,
         )
+        response = requests.get(image_address)
+        if response.status_code == 200:
+            image_content = ContentFile(response.content)
+            user_instance.profile_image.save(f"{login_id}.png", image_content)
+        user_instance.save()
+        return redirect("http://127.0.0.1:8000/users/")
 
     def _get_access_token(self, request):
         load_dotenv()
-        grant_type = 'authorization_code'
+        grant_type = "authorization_code"
         client_id = os.environ.get("CLIENT_ID")
         client_secret = os.environ.get("CLIENT_SECRET")
-        code = request.GET.get('code')
-        state = request.GET.get('state')
+        code = request.GET.get("code")
+        state = request.GET.get("state")
         redirect_uri = "http://127.0.0.1:8000/user/42/callback"
-        data = {"grant_type": grant_type, "client_id": client_id, "client_secret": client_secret, "code": code,
-                "state": state, "redirect_uri": redirect_uri}
+        data = {
+            "grant_type": grant_type,
+            "client_id": client_id,
+            "client_secret": client_secret,
+            "code": code,
+            "state": state,
+            "redirect_uri": redirect_uri,
+        }
         token_request = requests.post("https://api.intra.42.fr/oauth/token", data)
         token_response_json = token_request.json()
         return token_response_json.get("access_token")
