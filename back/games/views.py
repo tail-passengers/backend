5715ad0f39d00bc545886e1828dd9e84ab2a659e
Kdelphinus <kmj951015@gmail.com>
2024-02-24 06:27:08 +0900

    :sparkles: user_id로 filter되어 나오도록 get 오버라이딩 및 debug용 post 추가
    Signed-off-by: Kdelphinus <kmj951015@gmail.com>

--- back/games/views.py
+++ back/games/views.py
@@ -1,5 +1,7 @@
 from django.db import IntegrityError
+from django.db.models import Q
 from rest_framework import viewsets
+from rest_framework.response import Response
 from rest_framework.exceptions import ValidationError
 from .serializers import (
     GeneralGameLogsSerializer,
@@ -18,6 +20,18 @@ from .models import (
 class GeneralGameLogsViewSet(viewsets.ModelViewSet):
     queryset = GeneralGameLogs.objects.all()
     serializer_class = GeneralGameLogsSerializer
+    http_method_names = ["get", "post"]  # TODO debug를 위해 post 임시 추가
+
+    def list(self, request, *args, **kwargs):
+        # 밑에 if문은 debug를 위한 임시 get
+        if "fk" not in kwargs:
+            return super().list(request, *args, **kwargs)
+
+        user_id = kwargs["fk"]
+        # Q 객체는 복잡한 쿼리 조건을 사용할 때 사용
+        queryset = self.queryset.filter(Q(winner=user_id) | Q(loser=user_id))
+        serializer = self.serializer_class(queryset, many=True)
+        return Response(serializer.data)
 
 
 class JoinGeneralGameViewSet(viewsets.ModelViewSet):
