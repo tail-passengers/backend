1d7b66a899ce9967bcaf79388ffd165b6125fc60
Kdelphinus <kmj951015@gmail.com>
2024-02-24 11:44:53 +0900

    :art: api에 맞춰서 url 세팅
    Signed-off-by: Kdelphinus <kmj951015@gmail.com>

--- back/friendrequests/urls.py
+++ back/friendrequests/urls.py
@@ -7,5 +7,13 @@ router = routers.DefaultRouter()
 router.register("friend_requests", views.FriendRequestViewSet)
 
 urlpatterns = [
+    path(
+        "friend_requests/<uuid:user_id>",
+        views.FriendListViewSet.as_view({"get": "list"}),
+    ),
+    path(
+        "friend_requests/",
+        views.FriendRequestViewSet.as_view({"get": "list", "post": "create"}),
+    ),
     path("", include(router.urls)),
 ]
--- back/games/urls.py
+++ back/games/urls.py
@@ -7,9 +7,22 @@ from . import views
 router = routers.DefaultRouter()
 router.register("general_game_logs", views.GeneralGameLogsViewSet)
 router.register("tournament_game_logs", views.TournamentGameLogsViewSet)
-router.register("join_general_game", views.JoinGeneralGameViewSet)
-router.register("join_tournament_game", views.JoinTournamentGameViewSet)
+# API 지원 안 함
+# router.register("join_general_game", views.JoinGeneralGameViewSet)
+# router.register("join_tournament_game", views.JoinTournamentGameViewSet)
 
 urlpatterns = [
+    path(
+        "general_game_logs/<uuid:fk>/",
+        views.GeneralGameLogsViewSet.as_view({"get": "list"}),
+    ),
+    path(
+        "tournament_game_logs/<uuid:fk>/",
+        views.TournamentGameLogsViewSet.as_view({"get": "list"}),
+    ),
+    path(
+        "tournament_game_logs/<str:name>/",
+        views.TournamentGameLogsViewSet.as_view({"get": "list"}),
+    ),
     path("", include(router.urls)),
 ]
