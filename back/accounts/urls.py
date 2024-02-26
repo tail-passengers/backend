2517055e5032e8d905b3ecbc011e056c219e3a79
gyuhong <qjwb2@naver.com>
2024-02-26 15:10:07 +0900

    :ambulance: accounts test 관련 url 수정

--- back/accounts/urls.py
+++ back/accounts/urls.py
@@ -4,18 +4,23 @@ from . import views
 from .views import logout_view
 
 # DefaultRouter
-router = routers.DefaultRouter()
-router.register("users", views.UsersViewSet)
+# router = routers.DefaultRouter()
+# router.register("users", views.UsersViewSet)
 
 urlpatterns = [
+    path(
+        "users/",
+        views.UsersViewSet.as_view({"get": "list", "post": "create"}),
+        name="users",
+    ),
     path(
         "users/<uuid:pk>/",
         views.UsersDetailViewSet.as_view(
             {"get": "list", "patch": "partial_update", "delete": "destroy"}
         ),
+        name="usersDetail",
     ),
     path("login/", views.Login42APIView.as_view()),
     path("login/42/callback/", views.CallbackAPIView.as_view()),
     path("logout/", logout_view, name="logout"),
-    path("", include(router.urls)),
 ]
