bd99a0c8c919b1edc8b8c091d4d8c9037bc63afa
Kdelphinus <kmj951015@gmail.com>
2024-02-27 19:53:57 +0900

    :art: friend_requests 에 대한 url 정리
    Signed-off-by: Kdelphinus <kmj951015@gmail.com>

--- back/friendrequests/urls.py
+++ back/friendrequests/urls.py
@@ -3,31 +3,24 @@ from . import views
 
 urlpatterns = [
     path(
-        "friend_requests/<uuid:user_id>/",
+        "friend_requests/<uuid:user_id>/<str:status>/",
         views.FriendListViewSet.as_view({"get": "list"}),
     ),
     path(
-        "friend_requests/<uuid:user_id>/<uuid:request_id>/",
+        "friend_requests/<uuid:request_id>/",
         views.FriendRequestDetailViewSet.as_view(
             {
-                "get": "list",
                 "patch": "partial_update",
                 "delete": "destroy",
             }
         ),
     ),
     path(
-        "friend_requests/<uuid:user_id>/<str:status>/",
-        views.FriendListViewSet.as_view({"get": "list"}),
-    ),
-    path(
         "friend_requests/",
         views.FriendRequestViewSet.as_view(
             {
-                "get": "list",
                 "post": "create",
             }
         ),
     ),
-    # path("", views.FriendRequestViewSet.as_view({"get": "list"})),
 ]
