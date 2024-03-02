41eba9a8f0ae3a2850f6e6324d492904dad9e401
Kdelphinus <kmj951015@gmail.com>
2024-03-02 22:18:21 +0900

    :art: get, post에서 intra_id를 사용하도록 변경 및 이에 따른 serializer 추가
    Signed-off-by: Kdelphinus <kmj951015@gmail.com>

--- back/games/views.py
+++ back/games/views.py
@@ -1,3 +1,5 @@
+import uuid
+
 from django.core.exceptions import ObjectDoesNotExist
 from django.db import IntegrityError, DatabaseError
 from django.db.models import Q
@@ -8,7 +10,9 @@ from rest_framework.exceptions import ValidationError
 from accounts.models import Users
 from .serializers import (
     GeneralGameLogsSerializer,
+    GeneralGameLogsListSerializer,
     TournamentGameLogsSerializer,
+    TournamentGameLogsListSerializer,
     JoinGeneralGameSerializer,
     JoinTournamentGameSerializer,
 )
@@ -20,17 +24,69 @@ from .models import (
 )
 
 
+def is_exist_user(key, value):
+    try:
+        user = None
+        if key == "intra_id":
+            user = Users.objects.get(intra_id=value)
+        elif key == "user_id":
+            uuid.UUID(value)  # UUID 형식인지 확인, 안하면 get 내부 함수에서 raise 발생
+            user = Users.objects.get(user_id=value)
+        return user
+    except (ObjectDoesNotExist, ValueError):
+        return None
+
+
+def get_user_from_intra_id_or_user_id(ids):
+    """
+    intra_id 또는 user_id로 유저를 찾아 반환
+    """
+    user = is_exist_user("intra_id", ids)
+    if user is None:
+        user = is_exist_user("user_id", ids)
+        if user is None:
+            raise ValidationError({"error": "유저가 존재하지 않습니다."})
+    return user
+
+
+def create_with_intra_id_convert_to_user_id(self, request):
+    """
+    intra_id를 user_id로 변환하여 Game Log를 생성
+    """
+    winner_intra_id = request.data.get("winner")
+    loser_intra_id = request.data.get("loser")
+
+    winner_user = get_user_from_intra_id_or_user_id(winner_intra_id)
+    loser_user = get_user_from_intra_id_or_user_id(loser_intra_id)
+
+    request_copy_data = request.data.copy()
+    request_copy_data["winner"] = winner_user.user_id
+    request_copy_data["loser"] = loser_user.user_id
+
+    serializer = self.get_serializer(data=request_copy_data)
+    serializer.is_valid(raise_exception=True)
+    self.perform_create(serializer)
+    headers = self.get_success_headers(serializer.data)
+    return (
+        winner_user,
+        loser_user,
+        Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers),
+    )
+
+
 class GeneralGameLogsViewSet(viewsets.ModelViewSet):
     permission_classes = [IsAuthenticated]
     queryset = GeneralGameLogs.objects.all()
     serializer_class = GeneralGameLogsSerializer
-    http_method_names = ["get", "post"]  # TODO debug를 위해 post 임시 추가
+    http_method_names = ["get", "post"]  # TODO debug를 위해 get 임시 추가
 
     # general game logs 생성 시 join general game 생성하는 오버라이딩 에러 발생
     # fk는 uuid가 아닌 인스턴스를 요구해서 생긴 에러인듯
     def create(self, request, *args, **kwargs):
         try:
-            response = super().create(request, *args, **kwargs)
+            winner_user, loser_user, response = create_with_intra_id_convert_to_user_id(
+                self, request
+            )
             game_id = response.data["game_id"]
             if not game_id:
                 return Response(
@@ -38,23 +94,8 @@ class GeneralGameLogsViewSet(viewsets.ModelViewSet):
                     status=status.HTTP_500_INTERNAL_SERVER_ERROR,
                 )
             game_instance = GeneralGameLogs.objects.get(game_id=game_id)
-            winner_uuid = request.data.get("winner")
-            loser_uuid = request.data.get("loser")
-            if winner_uuid and loser_uuid:
-                try:
-                    winner_user = Users.objects.get(user_id=winner_uuid)
-                    loser_user = Users.objects.get(user_id=loser_uuid)
-                except ObjectDoesNotExist:
-                    return Response(
-                        {"error": "유저가 존재하지 않습니다."},
-                        status=status.HTTP_404_NOT_FOUND,
-                    )
-                JoinGeneralGame.objects.create(
-                    game_id=game_instance, user_id=winner_user
-                )
-                JoinGeneralGame.objects.create(
-                    game_id=game_instance, user_id=loser_user
-                )
+            JoinGeneralGame.objects.create(game_id=game_instance, user_id=winner_user)
+            JoinGeneralGame.objects.create(game_id=game_instance, user_id=loser_user)
             return response
 
         except IntegrityError as e:
@@ -64,14 +105,24 @@ class GeneralGameLogsViewSet(viewsets.ModelViewSet):
                 {"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR
             )
 
+
+class GeneralGameLogsListViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticated]
+    queryset = GeneralGameLogs.objects.all()
+    serializer_class = GeneralGameLogsListSerializer
+
     def list(self, request, *args, **kwargs):
         # 밑에 if문은 debug를 위한 임시 get
-        if "fk" not in kwargs:
+        if "intra_id" not in kwargs:
             return super().list(request, *args, **kwargs)
 
-        user_id = kwargs["fk"]
-        # Q 객체는 복잡한 쿼리 조건을 사용할 때 사용, 밑에는 or 조건
-        queryset = self.queryset.filter(Q(winner=user_id) | Q(loser=user_id))
+        user = get_user_from_intra_id_or_user_id(kwargs["intra_id"])
+        if user is None:
+            return Response(
+                {"error": "유저가 존재하지 않습니다."},
+                status=status.HTTP_404_NOT_FOUND,
+            )
+        queryset = self.queryset.filter(Q(winner=user.user_id) | Q(loser=user.user_id))
         serializer = self.serializer_class(queryset, many=True)
         return Response(serializer.data)
 
@@ -93,11 +144,13 @@ class TournamentGameLogsViewSet(viewsets.ModelViewSet):
     permission_classes = [IsAuthenticated]
     queryset = TournamentGameLogs.objects.all()
     serializer_class = TournamentGameLogsSerializer
-    http_method_names = ["get", "post"]  # TODO debug를 위해 post 임시 추가
+    http_method_names = ["get", "post"]  # TODO debug를 위해 get 임시 추가
 
     def create(self, request, *args, **kwargs):
         try:
-            response = super().create(request, *args, **kwargs)
+            winner_user, loser_user, response = create_with_intra_id_convert_to_user_id(
+                self, request
+            )
             tournament_name = response.data["tournament_name"]
             if not tournament_name:
                 return Response(
@@ -108,24 +161,10 @@ class TournamentGameLogsViewSet(viewsets.ModelViewSet):
                 tournament_name=tournament_name,
                 round=response.data["round"],
             )
-            tournament_name = request.data.get("tournament_name")
-            winner_uuid = request.data.get("winner")
-            loser_uuid = request.data.get("loser")
-            if winner_uuid and loser_uuid and tournament_name:
-                try:
-                    winner_user = Users.objects.get(user_id=winner_uuid)
-                    loser_user = Users.objects.get(user_id=loser_uuid)
-                except ObjectDoesNotExist:
-                    return Response(
-                        {"error": "유저가 존재하지 않습니다."},
-                        status=status.HTTP_404_NOT_FOUND,
-                    )
-                JoinTournamentGame.objects.create(
-                    game_id=game_instance, user_id=winner_user
-                )
-                JoinTournamentGame.objects.create(
-                    game_id=game_instance, user_id=loser_user
-                )
+            JoinTournamentGame.objects.create(
+                game_id=game_instance, user_id=winner_user
+            )
+            JoinTournamentGame.objects.create(game_id=game_instance, user_id=loser_user)
             return response
 
         except IntegrityError as e:
@@ -135,18 +174,29 @@ class TournamentGameLogsViewSet(viewsets.ModelViewSet):
                 {"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR
             )
 
+
+class TournamentGameLogsListViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticated]
+    queryset = TournamentGameLogs.objects.all()
+    serializer_class = TournamentGameLogsListSerializer
+
     def list(self, request, *args, **kwargs):
         # 밑에 if문은 debug를 위한 임시 get
-        print(kwargs)
-        if "fk" not in kwargs and "name" not in kwargs:
+        if "intra_id" not in kwargs and "name" not in kwargs:
             return super().list(request, *args, **kwargs)
 
-        if "fk" in kwargs and "name" not in kwargs:
-            user_id = kwargs["fk"]
-            queryset = self.queryset.filter(Q(winner=user_id) | Q(loser=user_id))
-        elif "name" in kwargs and "fk" not in kwargs:
-            name = kwargs["name"]
-            queryset = self.queryset.filter(tournament_name=name)
+        if "intra_id" in kwargs and "name" not in kwargs:
+            user = get_user_from_intra_id_or_user_id(kwargs["intra_id"])
+            if user is None:
+                return Response(
+                    {"error": "유저가 존재하지 않습니다."},
+                    status=status.HTTP_404_NOT_FOUND,
+                )
+            queryset = self.queryset.filter(
+                Q(winner=user.user_id) | Q(loser=user.user_id)
+            )
+        elif "name" in kwargs and "intra_id" not in kwargs:
+            queryset = self.queryset.filter(tournament_name=kwargs["name"])
         else:
             raise ValidationError({"detail": "잘못된 요청입니다."})
         serializer = self.serializer_class(queryset, many=True)
