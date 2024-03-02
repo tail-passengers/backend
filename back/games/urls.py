99ab4f820d1f610fd45e61863e54e12dd7db3ec3
Kdelphinus <kmj951015@gmail.com>
2024-03-02 22:17:48 +0900

    :art: url 경로를 intra_id로 변경
    Signed-off-by: Kdelphinus <kmj951015@gmail.com>

--- back/games/urls.py
+++ back/games/urls.py
@@ -9,23 +9,23 @@ urlpatterns = [
         name="general_game_logs",
     ),
     path(
-        "general_game_logs/<uuid:fk>/",
-        views.GeneralGameLogsViewSet.as_view({"get": "list"}),
+        "general_game_logs/<str:intra_id>/",
+        views.GeneralGameLogsListViewSet.as_view({"get": "list"}),
         name="general_game_logs_detail",
     ),
     path(
         "tournament_game_logs/",
-        views.TournamentGameLogsViewSet.as_view({"post": "create"}),
+        views.TournamentGameLogsViewSet.as_view({"get": "list", "post": "create"}),
         name="create_tournament_game_logs",
     ),
     path(
-        "tournament_game_logs/<uuid:fk>/",
-        views.TournamentGameLogsViewSet.as_view({"get": "list"}),
+        "tournament_game_logs/users/<str:intra_id>/",
+        views.TournamentGameLogsListViewSet.as_view({"get": "list"}),
         name="tournament_game_user_logs",
     ),
     path(
-        "tournament_game_logs/<str:name>/",
-        views.TournamentGameLogsViewSet.as_view({"get": "list"}),
+        "tournament_game_logs/tournament/<str:name>/",
+        views.TournamentGameLogsListViewSet.as_view({"get": "list"}),
         name="tournament_name_logs",
     ),
 ]
