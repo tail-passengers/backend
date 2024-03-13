6e04c5821779f0469a7b1fa5975ff1d5be0bb0e7
gyuhong <qjwb2@naver.com>
2024-03-13 17:40:17 +0900

    :art: general_game에 대한 기본적인 웹소켓 뼈대 추가

--- back/pong_game/consumers.py
+++ back/pong_game/consumers.py
@@ -19,3 +19,25 @@ class LoginConsumer(AsyncWebsocketConsumer):
     @database_sync_to_async
     def update_user_status(self, user, status):
         Users.objects.filter(user_id=user.user_id).update(status=status)
+
+
+class GeneralGameConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+        self.user = self.scope["user"]
+        if self.user.is_authenticated:
+            self.game_id = self.scope["url_route"]["kwargs"]["game_id"]
+            self.game_group_name = f"game_{self.game_id}"
+            await self.channel_layer.group_add(self.game_group_name, self.channel_name)
+            await self.accept()
+        else:
+            await self.close()
+
+    async def disconnect(self, close_code):
+        if self.user.is_authenticated:
+            await self.channel_layer.group_discard(
+                self.game_group_name, self.channel_name
+            )
+
+    async def receive(self, text_data):
+        # 게임 로직 추가 필요
+        pass
