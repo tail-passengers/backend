ef7049cc0a8d9041c09d00adf31a3301cdc34556
Kdelphinus <kmj951015@gmail.com>
2024-03-15 19:16:54 +0900

    :ambulance: load_dotenv 추가

--- back/back/settings.py
+++ back/back/settings.py
@@ -13,12 +13,16 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 from pathlib import Path
 import os
 
+from dotenv import load_dotenv
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
+
+load_dotenv()
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
