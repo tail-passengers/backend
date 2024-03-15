64639920f54d772bb4d38857dd58dc678d6faeff
gyuhong <qjwb2@naver.com>
2024-03-15 19:09:39 +0900

    :lock: DJANGO_SECRET_KEY 변경 후 env로 가져오게 수정했습니다.

--- back/back/settings.py
+++ back/back/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-7s0%0a+=uv=#cy_$3s-7bdl5@6xuutff5cq)fwuk^dn%0-l*9l"
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
