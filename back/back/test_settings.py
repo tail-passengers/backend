ec81c3ec14ddba710d301bb6c8ed3eec96cdd620
gyuhong <qjwb2@naver.com>
2024-03-20 21:08:09 +0900

    :white_check_mark: GeneralGameConsumer ready, start 동작 간단 테스트 구현
    Co-authored-by: Kdelphinus <kmj951015@gmail.com>

--- back/back/test_settings.py
+++ back/back/test_settings.py
@@ -6,3 +6,12 @@ DATABASES = {
         "NAME": BASE_DIR / "db.sqlite3",
     }
 }
+
+CHANNEL_LAYERS = {
+    "default": {
+        "BACKEND": "channels_redis.core.RedisChannelLayer",
+        "CONFIG": {
+            "hosts": [("127.0.0.1", 6379)],
+        },
+    },
+}
