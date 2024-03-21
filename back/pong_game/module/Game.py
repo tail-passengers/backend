934c787fe65332332f40dac452768a634504acce
Myoung Jun Ko <kmj951015@gmail.com>
2024-03-21 22:05:09 +0900

    :sparkles: Player 객체 구현

--- back/pong_game/module/Game.py
+++ back/pong_game/module/Game.py
@@ -1,73 +1,53 @@
 import json
-from .Paddle import Paddle
+from .Player import Player
 from .Ball import Ball
-from .GameSetValue import PADDLE_CORRECTION, KeyboardInput, PlayerStatus
+from .GameSetValue import PlayerStatus
 
 
 class GeneralGame:
     def __init__(self):
-        self.player1: str | None = None
-        self.player2: str | None = None
+        self.ball: Ball = Ball()
+        self.player1: Player | None = None
+        self.player2: Player | None = None
         self.score1: int = 0
         self.score2: int = 0
-        self.status1: PlayerStatus = PlayerStatus.WAIT
-        self.status2: PlayerStatus = PlayerStatus.WAIT
-        self.paddle1: Paddle = Paddle()
-        self.paddle2: Paddle = Paddle()
-        self.ball: Ball = Ball()
 
     def set_player(self, player_intra_id: str) -> None:
         if self.player1 is None:
-            self.player1 = player_intra_id
+            self.player1 = Player(1, player_intra_id)
             return
 
         if self.player2 is None:
-            self.player2 = player_intra_id
+            self.player2 = Player(2, player_intra_id)
             return
 
-    def get_player(self, player_id: int) -> str | None:
-        if player_id == 1:
+    def get_player(self, number: int) -> Player | None:
+        if number == 1:
             return self.player1
-        elif player_id == 2:
+        elif number == 2:
             return self.player2
         return None
 
-    def set_ready(self, player_intra_id: str) -> None:
-        if player_intra_id == self.player1:
-            self.status1 = PlayerStatus.READY
-        elif player_intra_id == self.player2:
-            self.status2 = PlayerStatus.READY
+    def set_ready(self, number: str) -> None:
+        if number == "player1":
+            self.player1.set_status(PlayerStatus.READY)
+        elif number == "player2":
+            self.player2.set_status(PlayerStatus.READY)
 
     def get_ready(self) -> bool:
-        if self.status1 == self.status2 == PlayerStatus.READY:
+        if self.player1 is None or self.player2 is None:
+            return False
+        if self.player1.get_status() == self.player2.get_status() == PlayerStatus.READY:
             return True
         return False
 
     def reset_position(self) -> None:
-        self.paddle1.reset_position()
-        self.paddle2.reset_position()
         self.ball.reset_position()
 
-    def update_position(self, text_data: json) -> None:
-        data = json.load(text_data)
-        if data["intra_id"] == self.player1:
-            if data["keyboard_input"] == KeyboardInput.LEFT:
-                self.paddle1.left()
-            elif data["keyboard_input"] == KeyboardInput.RIGHT:
-                self.paddle1.right()
-            elif data["keyboard_input"] == KeyboardInput.SPACE:
-                self.paddle1.space()
-        elif data["intra_id"] == self.player2:
-            if data["keyboard_input"] == KeyboardInput.LEFT:
-                self.paddle2.left()
-            elif data["keyboard_input"] == KeyboardInput.RIGHT:
-                self.paddle2.right()
-            elif data["keyboard_input"] == KeyboardInput.SPACE:
-                self.paddle2.space()
-
-    def is_past_paddle(self, intra_id: str) -> bool:
-        if intra_id == self.player1:
-            return self.ball.position_z > self.paddle1.position_z + PADDLE_CORRECTION
-        elif intra_id == self.player2:
-            return self.ball.position_z < self.paddle2.position_z - PADDLE_CORRECTION
-        return False
+    def key_input(self, text_data: json) -> None:
+        data = json.loads(text_data)
+        number = self.get_player(data["number"])
+        if number == "1":
+            self.player1.move_paddle(data["input"])
+        elif number == "2":
+            self.player2.move_paddle(data["input"])
--- back/pong_game/module/Paddle.py
+++ back/pong_game/module/Paddle.py
@@ -1,22 +1,43 @@
-from .GameSetValue import FIELD_LENGTH
+from .GameSetValue import (
+    PADDLE_SPEED,
+    FIELD_WIDTH,
+    PADDLE_WIDTH,
+    FIELD_LENGTH,
+    KeyboardInput,
+)
 
 
 class Paddle:
-    def __init__(self):
+    def __init__(self, number: int):
         self.position_x: float = 0
         self.position_y: float = 0
-        self.position_z: float = FIELD_LENGTH / 2
-        self.width: float = 0
-        self.speed: float = 0
+        self.position_z: float = FIELD_LENGTH / 2 if number == 1 else -FIELD_LENGTH / 2
 
-    def reset_position(self):
-        self.position_x = 0
+        self.left: bool = False
+        self.right: bool = False
 
-    def left(self):
-        self.position_x -= self.speed
+        # TODO front 구현에 따라 필요할 수도 안 필요할 수도 있음
+        # self.direction: int = 1 if number == 1 else -1
 
-    def right(self):
-        self.position_x += self.speed
+    # TODO space 키는 프론트에서 처리 후, protego maxima 가 실행되었는지 판단하여 전달
+    def key_handler(self, key_input: str) -> None:
+        if key_input == KeyboardInput.LEFT_PRESS.value:
+            self.left = True
+        elif key_input == KeyboardInput.LEFT_RELEASE.value:
+            self.left = False
+        elif key_input == KeyboardInput.RIGHT_PRESS.value:
+            self.right = True
+        elif key_input == KeyboardInput.RIGHT_RELEASE.value:
+            self.right = False
 
-    def space(self):
-        pass
+        if self.left and not self.right:
+            self._move(1)
+        elif not self.left and self.right:
+            self._move(-1)
+
+    def _move(self, direction: int) -> None:
+        new_paddle_x = self.position_x + direction * PADDLE_SPEED
+        self.position_x = max(
+            -FIELD_WIDTH / 2 + PADDLE_WIDTH / 2,
+            min(FIELD_WIDTH / 2 - PADDLE_WIDTH / 2, new_paddle_x),
+        )
