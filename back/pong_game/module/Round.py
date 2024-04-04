fc1e858bffaff8efe48540f0010ec69d512aedcb
gyuhong <qjwb2@naver.com>
2024-04-04 20:49:34 +0900

    :bug: 필요한 함수들 추가
    Co-authored-by: Kdelphinus <kmj951015@gmail.com>

--- back/pong_game/module/Round.py
+++ back/pong_game/module/Round.py
@@ -61,17 +61,28 @@ class Round(GeneralGame):
             }
         )
 
+    def is_all_ready(self) -> bool:
+        if self.player1 is None or self.player2 is None:
+            return False
+        if (
+            self.player1.get_status()
+            == self.player2.get_status()
+            == PlayerStatus.ROUND_READY
+        ):
+            return True
+        return False
+
     def get_winner(self) -> str:
         return self.winner
 
     def get_is_closed(self) -> bool:
         return self.is_closed
 
-    def set_ready(self, intra_id: str) -> None:
+    def set_round_ready(self, intra_id: str) -> None:
         if intra_id == self.player1.get_intra_id():
-            self.player1.set_status(PlayerStatus.READY)
+            self.player1.set_status(PlayerStatus.ROUND_READY)
         elif intra_id == self.player2.get_intra_id():
-            self.player2.set_status(PlayerStatus.READY)
+            self.player2.set_status(PlayerStatus.ROUND_READY)
 
     def set_is_closed(self, is_closed: bool) -> None:
         self.is_closed = is_closed
